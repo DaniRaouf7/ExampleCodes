969476a1b4f53ffefbebd7c2aadbcbe7d6203b7f
DaniRaouf7 <danishraouf@hotmail.com>
2024-05-31 12:02:32 +0200

    First lines of coding

---
+++ src/main.py
@@ -0,0 +1,11 @@
+#Basic print statement
+print("Hello world!")
+
+# Print multiple outputs with different lines use \n
+print("hello world\nhello world")
+
+#input
+input("What is your name?")
+
+#Hello + input
+print("Hello " + input("What is your name?") + "!")
