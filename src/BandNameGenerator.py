269068c93940a7d135659b7d0843b7759c9620f8
DaniRaouf7 <danishraouf@hotmail.com>
2024-05-31 14:11:36 +0200

    Bandnamegenerator

---
+++ src/BandNameGenerator.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator.")
+city = input("What's the name of the city you grew up in?\n")
+name = input("What is your pet his/her name?\n")
+print("Your band name could be named " + city + " " + name + "!")
--- src/main.py
+++ src/main.py
@@ -10,3 +10,7 @@ input("What is your name?")
 # Hello + input
 print("Hello " + input("What is your name?") + "!")
 
+# Variable coding
+name = input("What is your name?")
+length = len(name)
+print(length)
