83d89736a1ad71233d691bcd0dc7ff15ae4dbddb
DaniRaouf7 <danishraouf@hotmail.com>
2024-05-31 13:34:08 +0200

    Some extra sample coding

---
+++ src/Test1.py
@@ -0,0 +1,6 @@
+
+# Simple function with input
+num1 = int(input())
+num2 = int(input())
+
+print(num1 * num2)
--- src/main.py
+++ src/main.py
@@ -1,11 +1,17 @@
-#Basic print statement
+# Basic print statement
 print("Hello world!")
 
 # Print multiple outputs with different lines use \n
 print("hello world\nhello world")
 
-#input
+# Input
 input("What is your name?")
 
-#Hello + input
+# Hello + input
 print("Hello " + input("What is your name?") + "!")
+
+# Simple function with input
+num1 = int(input())
+num2 = int(input())
+
+print(num1 * num2)
