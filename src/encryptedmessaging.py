63aeeaa42e74136fc074d67144b1435f8bf27ac3
DaniRaouf7 <danishraouf@hotmail.com>
2024-07-02 15:36:23 +0200

    New File Added

---
+++ src/encryptedmessaging.py
@@ -0,0 +1,20 @@
+# def greet(Name):
+#     print('Hallo' + Name)
+#     print('You are cool')
+#     print('Bye')
+
+# greet(" Dani!")
+
+# def greet_with_name(name):
+#     print(f'Hello {name}!!')
+#     print(f'You are so cool {name}!')
+    
+# greet_with_name("Dani")
+
+# # Parameter is name and argument is Dani
+
+def greet_with(name, location):
+    print(f'Hello {name}!')
+    print(f'How is it like in {location}?')
+    
+greet_with(name = "Dani",location = "Pijnacker")
