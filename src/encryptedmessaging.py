6b9fcacc9d9255ac467008f82f851a030f9e4228
DaniRaouf7 <danishraouf@hotmail.com>
2024-07-05 13:56:35 +0200

    New program that encrypts and decrypts text

--- src/encryptedmessaging.py
+++ src/encryptedmessaging.py
@@ -1,20 +1,70 @@
-# def greet(Name):
-#     print('Hallo' + Name)
-#     print('You are cool')
-#     print('Bye')
+from encodedtext_art import logo
 
-# greet(" Dani!")
+print(logo)
 
-# def greet_with_name(name):
-#     print(f'Hello {name}!!')
-#     print(f'You are so cool {name}!')
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+
+# direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+# text = input("Type your message:\n").lower()
+# shift = int(input("Type the shift number:\n"))
+
+# # Tackle the problem if they submit a shift number that is really large
+# shift = shift % 26
     
-# greet_with_name("Dani")
-
-# # Parameter is name and argument is Dani
-
-def greet_with(name, location):
-    print(f'Hello {name}!')
-    print(f'How is it like in {location}?')
+# def encrypt(plain_text, shift_amount):
+#     cipher_text = ""
+#     for letter in plain_text:
+#         position = alphabet.index(letter)
+#         new_position = position + shift_amount
+#         new_letter = alphabet[new_position]
+#         cipher_text += new_letter
+#     print(f'The encoded text is {cipher_text}')
     
-greet_with(name = "Dani",location = "Pijnacker")
+# def decrypt(cipher_text, shift_amount):
+#     decrypt_text = ""
+#     for letter in cipher_text:
+#         position = alphabet.index(letter)
+#         new_position = position - shift_amount
+#         new_letter = alphabet[new_position]
+#         decrypt_text += new_letter
+#     print(f'The decoded message is {decrypt_text}')
+    
+
+# if direction == "encode":
+#     encrypt(plain_text=text, shift_amount=shift)
+# else:
+#     decrypt(cipher_text=text, shift_amount=shift)
+
+
+def ceaser(start_text, shift_amount, cipher_direction):
+    end_text = ""
+    if cipher_direction == 'decode':
+        shift_amount *= -1
+    for char in start_text:
+        if char in alphabet:
+            position = alphabet.index(char)
+            new_position = position + shift_amount
+            end_text += alphabet[new_position]
+        else:
+            end_text += char
+        
+    print(f'The {cipher_direction}d text is {end_text}')
+    
+# ceaser(start_text=text, shift_amount=shift,cipher_direction=direction)
+    
+should_continue = True
+
+while should_continue:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+
+# Tackle the problem if they submit a shift number that is really large
+    shift = shift % 26
+    ceaser(start_text=text, shift_amount=shift,cipher_direction=direction)
+    
+    result = input("Type 'yes' if you want to go again. Otherwise type 'no'\n")
+    if result == 'no':
+        should_continue = False
+        print('Goodbye')
